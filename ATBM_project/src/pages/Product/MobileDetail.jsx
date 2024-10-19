f20209e50806f6fac7d31b727fa567195b12b7ad
AnhHong8323 <hongoni8323@gmail.com>
2024-10-19 15:51:07 +0700

    Update Mobile Detail

--- ATBM_project/src/pages/Product/MobileDetail.jsx
+++ ATBM_project/src/pages/Product/MobileDetail.jsx
@@ -1,44 +1,43 @@
-import React from 'react'
-import { Container, Paper, Grid, Box } from '@mui/material'; 
-import { makeStyles } from '@mui/styles';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import './style.css';
+import React from "react";
+import { Container, Paper, Grid, Box } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "./style.css";
+import Slider from "../../components/Slider/Slider";
 
 const useStyles = makeStyles((theme) => ({
-    left:{
-        width:'50%',
-    },
-    right:{
-        flex:'1 1 0',
-    },
-   
+  left: {
+    width: "50%",
+  },
+  right: {
+    flex: "1 1 0",
+  },
 }));
 const MobileDetail = () => {
-const classes = useStyles();
+  const classes = useStyles();
   return (
     <Box>
-        <Container>
-             <Paper elevation={0}>
-                <Grid container>
-                    <Grid item className={classes.left}>
-                        Thumbnail
-                        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                            <SwiperSlide>Slide 1</SwiperSlide>
-                            <SwiperSlide>Slide 2</SwiperSlide>
-                            <SwiperSlide>Slide 3</SwiperSlide>
-                        </Swiper>
-                    </Grid>
-                    <Grid item className={classes.left}>
-                        Product Info
-                    </Grid>
-                </Grid>
-             </Paper>
-        </Container>
+      <Container>
+        <Paper elevation={0}>
+          <Grid container>
+            <Grid item className={classes.left}>
+              <Slider
+                url1="/images/image-removebg-preview-12.png"
+                url2="/images/image-removebg-preview-12.png"
+                url3="/images/image-removebg-preview-12.png"
+              />
+            </Grid>
+            <Grid item className={classes.left}>
+              Product Info
+            </Grid>
+          </Grid>
+        </Paper>
+      </Container>
     </Box>
-  )
-}
-export default MobileDetail
+  );
+};
+export default MobileDetail;
